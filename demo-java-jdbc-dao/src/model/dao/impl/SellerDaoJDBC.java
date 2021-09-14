0aedaf70b3398065c8cef03d2555fc197cf36f40
RamonBecker <ramonbecker68@gmail.com>
2021-09-14 19:39:19 -0300

    Implementando findByDepartment na classe SellerDaoJDBC

--- demo-java-jdbc-dao/src/model/dao/impl/SellerDaoJDBC.java
+++ demo-java-jdbc-dao/src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DBException;
@@ -66,6 +69,52 @@ public class SellerDaoJDBC implements SellerDao {
 		}
 	}
 
+	@Override
+	public List<Seller> findAll() {
+		return null;
+	}
+
+	@Override
+	public List<Seller> findByDeparment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
+
+			st.setInt(1, department.getId());
+
+			rs = st.executeQuery();
+
+			List<Seller> list = new ArrayList<Seller>();
+			Map<Integer, Department> map = new HashMap<Integer, Department>();
+
+			while (rs.next()) {
+
+				Department dep = map.get(rs.getInt("DepartmentId"));
+
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(dep.getId(), dep);
+				}
+
+				Seller seller = instantiateSeller(rs, dep);
+				list.add(seller);
+				// return seller;
+			}
+			return list;
+
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			DB.closeConnection();
+		}
+	}
+
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
 		Seller seller = new Seller();
 		seller.setId(rs.getInt("Id"));
@@ -84,8 +133,4 @@ public class SellerDaoJDBC implements SellerDao {
 		return dep;
 	}
 
-	@Override
-	public List<Seller> findAll() {
-		return null;
-	}
 }
