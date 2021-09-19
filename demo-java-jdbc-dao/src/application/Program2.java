b25e9891d7660b89954c4af813ea0ce43fa604c4
RamonBecker <ramonbecker68@gmail.com>
2021-09-18 21:36:40 -0300

    Implementação findById na classe de DepartmentDaoJDBC

--- demo-java-jdbc-dao/src/application/Program2.java
+++ demo-java-jdbc-dao/src/application/Program2.java
@@ -2,28 +2,43 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
-import model.dao.impl.DepartmentDaoJDBC;
 import model.entities.Department;
 
 public class Program2 {
 
-	
 	private static DepartmentDao departmentDao = null;
 	private static Department department = null;
-	
+
 	public static void main(String[] args) {
-		inserir();
+//		insert();
+		
+		findByID();
 	}
-	
-	private static void inserir() {
+
+	private static void insert() {
 		System.out.println("=== Teste 1: Department INSERT ===");
-		
+
 		departmentDao = DaoFactory.createDeparmentDao();
-		
+
 		department = new Department();
 		department.setName("Financial");
-		departmentDao.insert(department);
 		
-		System.out.println("Inserted! New id = "+department.getId());
+		departmentDao.insert(department);
+
+		System.out.println("Inserted! New id = " + department.getId());
+	}
+
+	private static void findByID() {
+		System.out.println("=== Test 2: Department FindByID ===");
+
+		departmentDao = DaoFactory.createDeparmentDao();
+
+		department = departmentDao.findById(1);
+		
+		if(department != null) {
+			System.out.println("Deparment found: "+department);
+		}else {
+			System.out.println("Not found deparment!");
+		}
 	}
 }
--- demo-java-jdbc-dao/src/model/dao/impl/DepartmentDaoJDBC.java
+++ demo-java-jdbc-dao/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -68,8 +68,32 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			st = conn.prepareStatement("SELECT Id, Name "
+					                 + "FROM department "
+					                 + "WHERE Id = ?");
+			
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if(rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				
+				return dep;
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			DB.closeConnection();
+		}
 	}
 
 	@Override
@@ -78,4 +102,11 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 		return null;
 	}
 
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		return dep;
+	}
 }
